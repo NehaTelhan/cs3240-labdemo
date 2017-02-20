e23f22a359d9c1637481f32faefc1cbdd80c01a0
Neha Telhan <neha.telhan@thehumangeo.com>
2017-02-20 14:06:27 -0500

    Added a greeting(msg) function to hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello World")
+__author__ = 'Neha Telhan (nt7ab)'
+__date__ = 'Feb 20, 2017'
+
+def greeting(msg):
+    print(msg)
+
+
+
+
+if __name__ == "__main__":
+    msg = input("Please enter a message: ")
+    greeting(msg)
